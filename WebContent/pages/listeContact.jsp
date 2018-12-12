300f071e30df6be950966c3ca63c01915590853c
Fabien <fabien.michl@gmail.com>
2018-12-12 22:46:06 +0100

    Update : remove search

--- WebContent/pages/listeContact.jsp
+++ WebContent/pages/listeContact.jsp
@@ -87,26 +87,7 @@ $(document).ready(function() {
       <li><a HREF="creationContact.do"><bean:message key="link.add.contact"/></a></li>
       <li><a href="#">Liste des contactsGroups</a></li>
     </ul>
-    <!-- 
-    <fieldset>
-			<html:form action="/SearchContact">
-				<html:errors />
-				<table class="wwFormTable" >
-					<tr>
-						<td align="left"><html:text property="element" size="10"
-								maxlength="30" value="" /></td>
-					</tr>
-					
-
-					<tr>
-						<td align="right"><html:submit><bean:message key="form.validate"/></html:submit></td>
-					</tr>
-				</table>
-
-
-			</html:form>
-		</fieldset>
-    	-->
+    
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-log-in"></span></a></li>
     </ul>
@@ -115,99 +96,6 @@ $(document).ready(function() {
 
 	<div align="center">
 		<h1>Gestionnaire de contact</h1>
-		<!--  
-		<a HREF="creationContact.do"><bean:message key="link.add.contact"/></a>
-		<fieldset>
-			<legend><bean:message key="form.search.legend"/> </legend>
-			<html:form action="/SearchContact">
-				<html:errors />
-				<table>
-
-					<tr>
-
-						<td align="center" colspan="2"><font size="4"><bean:message key="form.title"/></font>
-					</tr>
-					<tr>
-						<td align="right"><bean:message key="form.element"/></td>
-						<td align="left"><html:text property="element" size="30"
-								maxlength="30" value="" /></td>
-					</tr>
-					
-
-					<tr>
-						<td align="right"><html:submit><bean:message key="form.validate"/></html:submit></td>
-					</tr>
-				</table>
-
-
-			</html:form>
-		</fieldset>
-		-->
-		<!-- 
-		<ul>
-			<logic:iterate name="listContactsJDBC" id="itemJDBC">
-				<li>
-					<p id="notDisplay">
-						<bean:write name="itemJDBC" property="id" />
-					</p> 
-					<b><bean:message key="form.firstname"/></b><bean:write name="itemJDBC" property="firstName" /> , 
-					<b><bean:message key="form.lastname"/></b><bean:write name="itemJDBC" property="lastName" /> , 
-					<b><bean:message key="form.email"/></b><bean:write name="itemJDBC" property="email" /> 
-					<br> 
-					<html:form action="/RemoveContact">
-						<html:errors />
-						<table id="notDisplay">
-							<tr>
-								<td><html:text name="itemJDBC" property="id" size="30"
-										maxlength="30" /></td>
-							</tr>
-							<tr>
-								<td><html:text name="itemJDBC" property="email" size="30"
-										maxlength="30" /></td>
-							</tr>
-
-						</table>
-						<tr>
-							<td align="right"><html:submit><bean:message key="form.validate.remove"/></html:submit></td>
-						</tr>
-
-					</html:form> 
-					<html:form action="/AlterContact2">
-						<html:errors />
-						<table id="notDisplay">
-
-							<tr>
-
-								<td align="center" colspan="2"><font size="4"><bean:message key="form.title"/></font>
-							</tr>
-							<tr>
-								<td><html:text name="itemJDBC" property="id" size="30"
-										maxlength="30" /></td>
-							</tr>
-							<tr>
-								<td><html:text name="itemJDBC" property="firstName"
-										size="30" maxlength="30" /></td>
-							</tr>
-							<tr>
-								<td><html:text name="itemJDBC" property="lastName"
-										size="30" maxlength="30" /></td>
-							</tr>
-
-							<tr>
-								<td><html:text name="itemJDBC" property="email" size="30"
-										maxlength="30" /></td>
-							</tr>
-
-						</table>
-						<tr>
-							<td align="right"><html:submit><bean:message key="form.validate.alter"/></html:submit></td>
-						</tr>
-					</html:form>
-
-				</li>
-			</logic:iterate>
-		</ul>
-	-->
 	</div>
 
 	<div>
--- src/domain/DAOContact.java
+++ src/domain/DAOContact.java
@@ -26,50 +26,16 @@ public class DAOContact extends DAO {
         super();
     }
 	
-	public Long countContact() throws SQLException{
-		Long size = (long) 0;
-		Context lContext;
-		try {
-			lContext = new InitialContext();
-			final DataSource lDataSource = (DataSource) lContext.lookup(RESOURCE_JDBC);
-			final Connection lConnection  = lDataSource.getConnection();
-			
-			final PreparedStatement lPreparedStatementCreation = 
-			lConnection.prepareStatement("SELECT MAX(ID_CONTACT) AS SIZE FROM CONTACT");
-			
-			
-			ResultSet result = lPreparedStatementCreation.executeQuery();
-			while(result.next()){
-				
-				try {
-					size = Long.parseLong(result.getString("SIZE"));
-					size++;
-					return size;
-			    } catch (NumberFormatException e) {
-			       return size;
-			    }
-			}
-			
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return size;
-		
-		
-	}
-	
 	public String addContact(final long id, final String firstName, final String lastName, final String email) {
 		String result = null;
-		String rq = "INSERT INTO CONTACT(ID_CONTACT, FIRSTNAME, LASTNAME, EMAIL) VALUES(?, ?, ?, ?)";
+		String rq = "INSERT INTO CONTACT(FIRSTNAME, LASTNAME, EMAIL) VALUES(?, ?, ?)";
 		try{
 			
 			super.setPreparedStatement(super.getContext().prepareStatement(rq, Statement.RETURN_GENERATED_KEYS));
 			PreparedStatement preparedStatement = super.getPreparedStatement();
-			preparedStatement.setLong(1, countContact());
-			preparedStatement.setString(2, firstName);
-			preparedStatement.setString(3, lastName);
-			preparedStatement.setString(4, email);
+			preparedStatement.setString(1, firstName);
+			preparedStatement.setString(2, lastName);
+			preparedStatement.setString(3, email);
 			super.setPreparedStatement(preparedStatement);
 			super.getPreparedStatement().executeUpdate();
 			super.close();
@@ -81,6 +47,46 @@ public class DAOContact extends DAO {
 		}
 		return result;
 		
+	}
+	
+	public String removeContact(final long id, final String email){
+		String result = null;
+		String rq = "DELETE FROM CONTACT WHERE ID_CONTACT="+id+" AND EMAIL='"+email+"'";
+		try{
+			super.setPreparedStatement(super.getContext().prepareStatement(rq));
+			super.getPreparedStatement().executeUpdate();
+			super.close();
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public Contact getContact(final long id){
+		Contact c = new Contact(id);
+		
+		String rq = "SELECT * FROM CONTACT WHERE ID_CONTACT= ? ;";
+		try{
+			
+			super.setPreparedStatement(super.getContext().prepareStatement(rq, Statement.RETURN_GENERATED_KEYS));
+			PreparedStatement preparedStatement = super.getPreparedStatement();
+			preparedStatement.setLong(1, id);
+			super.setPreparedStatement(preparedStatement);
+			ResultSet result = super.getPreparedStatement().executeQuery();
+			
+			while(result.next()){
+				c.setFirstName(result.getString("FIRSTNAME"));
+				c.setLastName(result.getString("LASTNAME"));
+				c.setEmail(result.getString("EMAIL"));
+			}
+			super.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return c;
 	}
 	
 	public String alterContact(final long id, final String firstName, final String lastName, final String email) {
@@ -105,20 +111,6 @@ public class DAOContact extends DAO {
 			return "SQLException : " + e.getMessage();
 			
 		}
-	}
-	public String removeContact(final long id, final String email){
-		String result = null;
-		String rq = "DELETE FROM CONTACT WHERE ID_CONTACT="+id+" AND EMAIL='"+email+"'";
-		try{
-			super.setPreparedStatement(super.getContext().prepareStatement(rq));
-			super.getPreparedStatement().executeUpdate();
-			super.close();
-		}
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
-		}
-		return result;
 	}
 	
 	public List<Contact> searchContact(String element){
@@ -253,6 +245,7 @@ public class DAOContact extends DAO {
 			
 			System.out.println("step 8 : DAOContact list contact : ");
 			
+			System.out.println(result);
 			
 			while(result.next()){
 				System.out.print("- "+result.getString("id_contact")+", ");
